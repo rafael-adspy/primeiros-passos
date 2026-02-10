5bcf7c94dde141f484315c7a30f55cab255c67b8
rafael-adspy <rafael145007@icloud.com>
2026-02-09 21:07:58 -0300

    Fix grade calculation by averaging scores

--- ex001.py
+++ ex001.py
@@ -1,7 +1,7 @@
 a = input(' Nome do Aluno? ')
 n1 = float(input(' Sua primeira nota e '))
 n2 = float(input(' Sua segunda nota e '))
-situacao = float( n1 + n2 )
+situacao = float( n1 + n2 ) / 2
 print (' O aluno {} teve nota {} e esta:'.format(a,situacao))
 if situacao >= 7:
     print('Aprovado')
@@ -10,3 +10,4 @@ elif situacao >= 5:
 else:
     print('Reprovado')
 # rint (' O aluno {} teve nota {}:'.format(a,situacao))
+
